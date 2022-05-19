299976e94d924c3b849f23aee00e034a4159f602
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2022-05-18 21:10:27 -0700

    Update karatsuba.py
    Define as a class

--- divide_and_conquer/karatsuba.py
+++ divide_and_conquer/karatsuba.py
@@ -10,38 +10,31 @@
 """
 import time
 
-def karatsuba(x, y):
-    a_string = str(x)
-    b_string = str(y)
-    if x < 10 or y < 10:
-        return x* y
+class Karatsuba:
 
-    max_length = max(len(a_string), len(b_string))
-    split_length = max_length // 2
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
 
-    # split string
-    a, b = int(a_string[:-split_length]), int(a_string[-split_length:])
-    c, d = int(b_string[:-split_length]), int(b_string[-split_length:])
+    def calc_karatsuba(x, y):
+        a_string = str(x)
+        b_string = str(y)
+        if x < 10 or y < 10:
+            return x* y
 
-    # print(a)
-    # print(b)
-    # print(c)
-    # print(d)
+        max_length = max(len(a_string), len(b_string))
+        split_length = max_length // 2
 
-    # z0 = a * c
-    # z1 = b * d
-    # z2 = (a + b) * (c + d)
+        # split string
+        a, b = int(a_string[:-split_length]), int(a_string[-split_length:])
+        c, d = int(b_string[:-split_length]), int(b_string[-split_length:])
 
-    # print(z0)
-    # print(z1)
-    # print(z2)
+        # spliting until the 2 digits multiplication
+        z0 = Karatsuba.calc_karatsuba(a, c)
+        z1 = Karatsuba.calc_karatsuba(b, d)
+        z2 = Karatsuba.calc_karatsuba((a +b), (c + d))
 
-    # spliting until the 2 digits multiplication
-    z0 = karatsuba(a, c)
-    z1 = karatsuba(b, d)
-    z2 = karatsuba((a +b), (c + d))
-
-    return (z0 * 10**(2*split_length)) + ((z2 - z1 - z0) * 10**split_length) + z1
+        return (z0 * 10**(2*split_length)) + ((z2 - z1 - z0) * 10**split_length) + z1
 
 
 if __name__ == '__main__':
@@ -50,7 +43,7 @@ if __name__ == '__main__':
     b = 2718281828459045235360287471352662497757247093699959574966967627
 
     start = time.time()
-    print(karatsuba(a, b))
+    print(Karatsuba.calc_karatsuba(a, b))
     print('Total time: %f' % (time.time() - start))
 
     # 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
